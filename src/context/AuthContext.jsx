9aa3db1af3a658e24339f621a865f4ce95647243
BeheadedBadger <ZarradeRuijter@gmail.com>
2024-07-20 21:15:07 +0200

    Fixed and implemented AuthContext

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,34 +1,101 @@
-import {createContext, useState} from "react";
-import axios from "axios";
+import { createContext, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { jwtDecode } from 'jwt-decode';
+import axios from 'axios';
 
-export const AuthContext = createContext({});
+export const AuthContext = createContext( {} );
 
-function AuthContextProvider({ children }) {
-    const [ isAuth, toggleIsAuth ] = useState({
-        isAuthenticated: false,
-        role: "",
-        user: "",
-        fetchUserData,
+function AuthContextProvider( { children } ) {
+    const [ isAuth, toggleIsAuth ] = useState( {
+        isAuth: false,
+        user: {username: null, role: null, speciality: null, favourites: null, shelterAnimals: [], donations: null},
+        status: 'pending',
     } );
+    const navigate = useNavigate();
 
-    async function fetchUserData( username ) {
-        try {
-            const result = await axios.get(`http://localhost:8080/users/${username}`);
+    useEffect( () => {
+        const token = localStorage.getItem( 'token' );
 
-            toggleIsAuth({
-                isAuthenticated: true,
-                user: result.data.username,
-                role: result.data.role,
-            });
-            console.log(result);
+        console.log( token === true );
+        if ( token === true ) {
+            console.log( token );
+            const decoded = jwtDecode( token );
+            void fetchUserData( decoded.sub, token );
+        } else {
+            toggleIsAuth( {
+                isAuth: false,
+                user: {username: null, role: null, speciality: null, favourites: null, shelterAnimals: []},
+                status: 'done',
+            } );
         }
-        catch (error) {
-            console.log(error);
+    }, [] );
+
+    function login( JWT ) {
+        localStorage.setItem( 'token', JWT );
+        const decoded = jwtDecode( JWT );
+
+        void fetchUserData( decoded.sub, JWT, '/profile' );
+    }
+
+    function logout() {
+        localStorage.clear();
+        toggleIsAuth( {
+            isAuth: false,
+            user: null,
+            status: 'done',
+        } );
+
+        console.log( 'User has been logged out!' );
+        navigate( '/' );
+    }
+
+    async function fetchUserData( id, token, redirectUrl ) {
+        try {
+            const result = await axios.get( `http://localhost:8080/users/${ id }`, {
+                headers: {
+                    "Content-Type": "application/json",
+                    Authorization: `Bearer ${ token }`,
+                },
+            } );
+
+            console.log( result.data );
+            toggleIsAuth({
+                isAuth: true,
+                user: {username: result.data.username,
+                    role: result.data.role,
+                    speciality: result.data.speciality,
+                    favourites: result.data.favouriteAnimals,
+                    shelterAnimals: result.data.shelterAnimals,
+                    donations: result.data.donations},
+                status: 'done',
+            } );
+
+            if ( redirectUrl ) {
+                navigate( redirectUrl );
+            }
+
+        } catch ( e ) {
+            console.error( e );
+            toggleIsAuth( {
+                isAuth: false,
+                user: null,
+                status: 'done',
+            } );
         }
     }
 
-    return <AuthContext.Provider value={isAuth}>
-        {children}
-    </AuthContext.Provider> }
+    const contextData = {
+        isAuth,
+        login,
+        logout
+    };
+
+    return (
+        <AuthContext.Provider value={ contextData }>
+            { isAuth.status === 'done' && children }
+            { isAuth.status === 'pending' && <p>Loading...</p> }
+        </AuthContext.Provider>
+    );
+}
 
 export default AuthContextProvider;
